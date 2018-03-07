bc9d430ab0f396ee7acdd7793efec6179dc7bee5
Konrad Kalicinski <konrad@innoveto.com>
2018-03-07 10:01:55 +0100

    adding 3rd content type

--- server/boot/init-content.js
+++ server/boot/init-content.js
@@ -3,16 +3,22 @@
 module.exports = function(app) {
   var Article = app.models.Article;
   var Secret = app.models.Secret;
+  var Mine = app.models.Mine;
 
-    for (var i = 0; i < 3; i++) {
-      Article.create({
-        title: 'Article #' + Math.round(Math.random() * 1000)
-      }, function(err, article) {
-      });
+  for (var i = 0; i < 3; i++) {
+    Article.create({
+      title: 'Article #' + Math.round(Math.random() * 1000),
+    }, function(err, article) {
+    });
 
-      Secret.create({
-        title: 'Secret #' + Math.round(Math.random() * 1000)
-      }, function(err, secret) {
-      });
-    }
+    Secret.create({
+      title: 'Secret #' + Math.round(Math.random() * 1000),
+    }, function(err, secret) {
+    });
+
+    Mine.create({
+      power: Math.round(Math.random() * 10000),
+    }, function(err, secret) {
+    });
+  }
 };
